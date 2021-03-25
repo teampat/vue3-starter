4baeaea99183b801c76b9a4ac49cce400d56c541
teampat <teampat@gmail.com>
2021-03-25 16:33:23 +0700

    Fix props

--- src/components/VueImage.vue
+++ src/components/VueImage.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="vue-image">
-    <img :src="src" :alt="alt">
+    <img :src="src" :alt="alt" />
   </div>
 </template>
 
 <script>
 export default {
-  name: "VueImage",
+  name: 'VueImage',
   props: {
-    src: String,
-    alt: String,
-  },
+    src: {
+      type: String,
+      required: true
+    },
+    alt: {
+      type: String,
+      required: true
+    }
+  }
 }
 </script>
