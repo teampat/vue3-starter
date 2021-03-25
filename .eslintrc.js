0f37335411ccfa99a4b87e6dfd82417c02097f3f
teampat <teampat@gmail.com>
2021-03-25 15:34:18 +0700

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/recommended',
+    '@vue/prettier',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['vue'],
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        semi: false,
+        trailingComma: 'none'
+      }
+    ],
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
